83f33988e02b9fa46a999e5b1e058cd14b6d80b6
Ваше Имя <you@example.com>
2020-11-12 10:54:46 +0300

    1

---
+++ shifr.cpp
@@ -0,0 +1,45 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string infile, outfile;
+    int shifr;
+    ifstream fin;
+    ofstream fout;
+    
+    cout << "Имя файла, который нужно обработать: ";
+    cin >> infile;
+    fin.open(infile);
+    if(!fin.is_open())
+    {
+        cout << "Ошибка открытия файла, который нужно обработать";
+        return 1;
+    }
+    
+    cout << "Имя файла результата: ";
+    cin >> outfile;
+    fout.open(outfile);
+    if(!fout.is_open())
+    {
+        cout << "Ошибка открытия файла результата";
+        return 2;
+    }
+    
+    
+    fin.close();
+    out.close();
+    return 0;
+}
+
